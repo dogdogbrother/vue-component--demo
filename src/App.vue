6c29298e100170b2cf1f82cc6681e8db0319a439
tsdhr1 <gougewh@sina.com>
2019-12-25 18:41:56 +0800

    form校验表单的实现

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <my-form></my-form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MyForm from './views/form'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    MyForm
   }
 }
 </script>
